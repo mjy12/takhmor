af7cf37b206918a59cbefe80afb12d9c5cdc7a29
mohammed usef <mjy1@hotmail.com>
2016-10-29 11:28:10 +0200

    fix cordova notification
    fix cordova notification

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -19192,14 +19192,14 @@ angular.module('mm.addons.remotestyles')
 angular.module('mm.core')
 
 .constant('mmCoreConfigConstants', {
-    "app_id" : "com.moodle.moodlemobile",
-    "versioncode" : "2016",
+    "app_id" : "edu.takhmor.mobile",
+    "versioncode" : "20160",
     "versionname" : "1.1",
     "cache_expiration_time" : 300000,
     "default_lang" : "ar",
     "languages": {"ar": "عربي", "bg": "Български", "ca": "Català", "cs": "Čeština", "de": "Deutsch","en": "English", "es": "Español", "es-mx": "Español - México", "eu": "Euskara", "fa": "فارسی", "fr" : "Français", "he" : "עברית", "hu": "magyar", "it": "Italiano", "ja": "日本語","nl": "Nederlands", "pl": "Polski", "pt-br": "Português - Brasil", "ru": "Русский", "sv": "Svenska", "tr" : "Türkçe", "zh-cn" : "简体中文", "zh-tw" : "正體中文"},
     "wsservice" : "moodle_mobile_app",
     "wsextservice" : "local_mobile",
-    "gcmpn": "694767596569"
+    "gcmpn": "718614473663"
 }
 );
